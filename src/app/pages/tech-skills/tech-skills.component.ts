b758c12bae382bf0e4b3de91fd81ccec941e2dfd
Ramon Trindade <ramonbraintrindade@gmail.com>
2023-11-05 21:29:10 -0300

    feat: tech-skill componente finish

--- src/app/pages/tech-skills/tech-skills.component.ts
+++ src/app/pages/tech-skills/tech-skills.component.ts
@@ -6,9 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./tech-skills.component.scss']
 })
 export class TechSkillsComponent {
-  progress1 = 50; // Define o progresso inicial da primeira barra
+  progress1 = 55;
+  progress2 = 55;
   imageAngular = 'assets/angular-icon.svg'
+  imageTypescript = 'assets/typescript--icon.svg'
   updateProgress1() {
     this.progress1 += 10;
+    this.progress2 += 10;
   }
 }
