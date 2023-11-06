68ff4891e6d0de7cd0bad5fd143716e01d81c85f
Ramon Trindade <ramonbraintrindade@gmail.com>
2023-11-06 01:19:58 -0300

    feat: tech-skills layout complete

--- src/app/pages/tech-skills/tech-skills.component.ts
+++ src/app/pages/tech-skills/tech-skills.component.ts
@@ -6,12 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./tech-skills.component.scss']
 })
 export class TechSkillsComponent {
-  progress1 = 55;
-  progress2 = 55;
+  progress1 = 82;
+  progress2 = 75;
+  progress3 = 65;
+  progress4 = 55;
+  progress5 = 45;
   imageAngular = 'assets/angular-icon.svg'
+  imageCSS = 'assets/css-icon.svg'
   imageTypescript = 'assets/typescript--icon.svg'
+  imageJavascript = 'assets/javascript-icon.svg'
+  imageHtml = 'assets/html-icon.svg'
   updateProgress1() {
     this.progress1 += 10;
     this.progress2 += 10;
+    this.progress3 += 10;
+    this.progress4 += 10;
+    this.progress5 += 10;
   }
 }
